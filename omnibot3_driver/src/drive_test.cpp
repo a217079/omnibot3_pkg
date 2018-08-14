7ae997a454dbe468a384a526a061da3ebacf347f
a217079 <a217079@ns.kogakuin.ac.jp>
2018-08-14 15:27:21 +0900

    [modify] add comment

--- omnibot3_driver/src/drive_test.cpp
+++ omnibot3_driver/src/drive_test.cpp
@@ -3,19 +3,19 @@
 
 #include <cmath>
 
-#define MAX_SPEED 3;
-#define STEP 0.01;
+#define MAX_SPEED 3
+#define STEP 0.01
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "drive_test");
   ros::NodeHandle n;
 
-  ros::Publisher wheel1_vel_pub = n.advertise<std_msgs::Float64.h>("omnibot3/wheel1_controller/command", 1000);
-  ros::Publisher wheel2_vel_pub = n.advertise<std_msgs::Float64.h>("omnibot3/wheel2_controller/command", 1000);
-  ros::Publisher wheel3_vel_pub = n.advertise<std_msgs::Float64.h>("omnibot3/wheel3_controller/command", 1000);
+  ros::Publisher wheel1_vel_pub = n.advertise<std_msgs::Float64>("omnibot3/wheel1_controller/command", 1000);
+  ros::Publisher wheel2_vel_pub = n.advertise<std_msgs::Float64>("omnibot3/wheel2_controller/command", 1000);
+  ros::Publisher wheel3_vel_pub = n.advertise<std_msgs::Float64>("omnibot3/wheel3_controller/command", 1000);
   
-  ros::Rate loop_rate(10)
+  ros::Rate loop_rate(10);
   
   int count = 0;
   while(ros::ok())
@@ -24,6 +24,7 @@ int main(int argc, char **argv)
     std_msgs::Float64 wheel2_vel;
     std_msgs::Float64 wheel3_vel;
     
+    // set each velocity (rad/s)
     wheel1_vel.data = MAX_SPEED * sin(count * STEP);
     wheel2_vel.data = 0;
     wheel3_vel.data = -1 * MAX_SPEED * sin(count * STEP);
